ae0557153c7c7df3f26ce8169acb4d2377de0f5a
Rajat Tyagi <40917672+tyagirajat200@users.noreply.github.com>
2020-06-26 18:23:10 +0530

    Cookies

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,16 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+  next();
+});
+
 
 
 const PORT = process.env.PORT || 4000
